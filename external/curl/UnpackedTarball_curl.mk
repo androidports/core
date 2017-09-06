61b200f23459809ec3ca87530287e3ff0b0eff8b
Stephan Bergmann <sbergman@redhat.com>
2017-09-06 23:28:46 +0200

    Pass --without-system-zlib into external/curl
    For (implicit) --with-zlib, curl's configure would insist on calling pkg-config
    and insist on using -lz to link against the zlib library.  None of that fits
    LO's --without-system-zlib option.
    
    So make curl's configure honor any passed in ZLIB_CLFAGS/LIBS (unless --without-
    zlib were explicitly specified; also, ZLIB_CLFAGS/LIBS would override any
    explicitly specified --with-zlib=PATH, which could be considered an error, but
    we do never explicitly specify --with-zlib=PATH in
    external/curl/ExternalProject_curl.mk, anyway).
    
    Both ZLIB_CLFAGS/LIBS are set in config_host.mk, so are always set as env vars,
    so would already be visible to curl's configure.  I nevertheless pass them into
    curl's configure explicitly in external/curl/ExternalProject_curl.mk, to make
    that more obvious.
    
    With (implicit) --with-system-zlib, LO's configure.ac will set ZLIB_LIBS and
    leave ZLIB_CFLAGS empty (and checks that zlib.h is indeed available without any
    ZLIB_CLFAGS being necessary).  And with --without-system-zlib, LO's configure.ac
    will set both ZLIB_CFLAGS/LIBS.  So curl's configure, as invoked from
    external/curl/ExternalProject_curl.mk, will now always go into the newly added
    case honoring any passed in ZLIB_CLFAGS/LIBS, which should thus always work out
    nicely and use the same zlib settings as configured for LO itself.
    
    (For documentation purposes, external/curl/zlib.patch.0 also contains the
    changes to curl's configure.ac that are reflected in the changes to curl's
    configure.)
    
    Change-Id: Icf5eefe44a7f9beb8a43a9af381f46e5c8b98b04
    Reviewed-on: https://gerrit.libreoffice.org/42032
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- external/curl/UnpackedTarball_curl.mk
+++ external/curl/UnpackedTarball_curl.mk
@@ -23,6 +23,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,curl,\
 	external/curl/curl-msvc-schannel.patch.1 \
 	external/curl/curl-7.26.0_win-proxy.patch \
 	external/curl/curl-xp.patch.1 \
+	external/curl/zlib.patch.0 \
 ))
 
 ifeq ($(SYSTEM_NSS),)
