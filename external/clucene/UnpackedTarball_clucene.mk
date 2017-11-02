251bbaa385b1b8c378f777ab42aa93effab415a5
AndroidPorts <androidports@users.noreply.github.com>
2017-11-02 10:08:46 +0900

    Clucene: Fix for AndroidPorts build.
    * external/clucene/Library_clucene.mk Override LIBS if LIBS is defined.
    * external/clucene/UnpackedTarball_clucene.mk Add path file clucene-config-android.h.
    * external/clucene/configs/_clucene-config-LINUX.h Use S_IRUSR S_IWUSR if S_IREAD S_IWRITE not defined.
    * external/clucene/configs/clucene-config-android.h Undef _CL_HAVE_SYS_TIMEB_H for Android.

--- external/clucene/UnpackedTarball_clucene.mk
+++ external/clucene/UnpackedTarball_clucene.mk
@@ -60,10 +60,14 @@ $(eval $(call gb_UnpackedTarball_add_file,clucene,src/shared/CLucene/clucene-con
 endif
 endif
 else # ! $(OS),WNT
+ifeq ($(ANDROID_PORTS),1)
+$(eval $(call gb_UnpackedTarball_add_file,clucene,src/shared/CLucene/clucene-config.h,external/clucene/configs/clucene-config-android.h))
+else
 ifeq ($(HAVE_GCC_BUILTIN_ATOMIC),TRUE)
 $(eval $(call gb_UnpackedTarball_add_file,clucene,src/shared/CLucene/clucene-config.h,external/clucene/configs/clucene-config-GCC-atomic.h))
 else
 $(eval $(call gb_UnpackedTarball_add_file,clucene,src/shared/CLucene/clucene-config.h,external/clucene/configs/clucene-config-generic.h))
+endif
 endif
 ifeq ($(OS),LINUX)
 $(eval $(call gb_UnpackedTarball_add_file,clucene,src/shared/CLucene/_clucene-config.h,external/clucene/configs/_clucene-config-LINUX.h))
