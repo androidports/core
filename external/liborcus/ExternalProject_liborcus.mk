69eca9d08dc0089bd8513b8abd6f6afa85452ac0
AndroidPorts <androidports@users.noreply.github.com>
2017-11-02 10:03:56 +0900

    Liborcus: Fix for AndroidPorts build.
    * external/liborcus/ExternalPackage_liborcus.mk Fix for suffix of library name for Android.
    * external/liborcus/ExternalProject_liborcus.mk Override CXXFLAGS if CXXFLAGS is defined.
    * external/liborcus/UnpackedTarball_liborcus.mk Add patch file for Android.
    * external/liborcus/androidports.patch Remove system library path from LDFLAGS.

--- external/liborcus/ExternalProject_liborcus.mk
+++ external/liborcus/ExternalProject_liborcus.mk
@@ -112,6 +112,7 @@ $(call gb_ExternalProject_get_state_target,liborcus,build) :
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		&& $(if $(verbose),V=1) \
 		   $(MAKE) \
+			CXXFLAGS='$(liborcus_CXXFLAGS) $(MDDS_CFLAGS)' \
 		$(if $(filter MACOSX,$(OS)),\
 			&& $(PERL) $(SRCDIR)/solenv/bin/macosx-change-install-names.pl shl OOO \
 				$(gb_Package_SOURCEDIR_liborcus)/src/liborcus/.libs/liborcus-0.12.0.dylib \
